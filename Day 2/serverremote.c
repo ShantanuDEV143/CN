8fc39f24646f8fcbb384f9846d773f59444888bb
ShantanuDEV143 <157054316+ShantanuDEV143@users.noreply.github.com>
2024-08-27 12:54:58 +0530

    Update serverremote.c

--- Day 2/serverremote.c
+++ Day 2/serverremote.c
@@ -12,12 +12,6 @@ int main() {
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
     char buffer[BUFFER_SIZE];
-    char server_ip[INET_ADDRSTRLEN];
-
-    // Input server IP address
-    printf("Enter server IP address: ");
-    fgets(server_ip, INET_ADDRSTRLEN, stdin);
-    server_ip[strcspn(server_ip, "\n")] = 0; // remove trailing newline
 
     // Create socket
     if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -28,12 +22,7 @@ int main() {
     // Define server address
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
-        perror("Invalid address/ Address not supported");
-        exit(EXIT_FAILURE);
-    }
+    inet_pton(AF_INET, "0.0.0.0", &server_addr.sin_addr); // Bind to all available network interfaces
 
     // Bind socket to address and port
     if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
@@ -47,7 +36,7 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    printf("Server listening on IP address %s and port %d...\n", server_ip, PORT);
+    printf("Server listening on port %d...\n", PORT);
 
     while (1) {
         // Accept incoming connection
@@ -59,10 +48,6 @@ int main() {
 
         printf("Connection accepted from client IP address %s and port %d...\n",
                inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
-        // Receive client name
-        recv(client_socket, buffer, BUFFER_SIZE, 0);
-        printf("Client name: %s\n", buffer);
 
         while (1) {
             // Receive message from client
